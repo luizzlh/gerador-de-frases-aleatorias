0925993bf22c3fd9e654e5662721a76fb32e318b
Luiz <luizzlh2004@gmail.com>
2026-01-03 16:13:09 -0300

    Controller criado

--- src/main/java/com/alura/frases/controller/FrasesController.java
+++ src/main/java/com/alura/frases/controller/FrasesController.java
@@ -1,10 +1,26 @@
 package com.alura.frases.controller;
 
+import com.alura.frases.DTO.SerieDTO;
+import com.alura.frases.service.FraseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/series/frases")
+@RestController()
+@RequestMapping("/series/frases")
 public class FrasesController {
+    @Autowired
+    private FraseService servico;
 
+//    @GetMapping("")
+//    public String index(){
+//        return "Teste";
+//    }
 
-
+    @GetMapping()
+    public SerieDTO obterFrase(){
+        return servico.obterFrase();
+    }
 }
