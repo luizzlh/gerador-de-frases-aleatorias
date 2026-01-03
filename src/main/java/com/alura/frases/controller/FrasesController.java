d0f55fa2d8209172b60f389556be25432abfb676
Luiz <luizzlh2004@gmail.com>
2026-01-03 15:30:55 -0300

    Iniciando RestController

--- src/main/java/com/alura/frases/controller/FrasesController.java
+++ src/main/java/com/alura/frases/controller/FrasesController.java
@@ -1,4 +1,10 @@
 package com.alura.frases.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/series/frases")
 public class FrasesController {
+
+
+
 }
