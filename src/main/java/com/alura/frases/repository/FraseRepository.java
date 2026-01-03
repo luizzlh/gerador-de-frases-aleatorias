e35c54a437da13fbfb55e5f7e818556438613cbb
Luiz <luizzlh2004@gmail.com>
2026-01-03 16:14:04 -0300

    Adicionado Repository da frase

--- src/main/java/com/alura/frases/repository/FraseRepository.java
+++ src/main/java/com/alura/frases/repository/FraseRepository.java
@@ -1,7 +1,13 @@
 package com.alura.frases.repository;
 
-import com.alura.frases.models.Frases;
+import com.alura.frases.models.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface FraseRepositorie extends JpaRepository<Frases, Long> {
+import java.util.Optional;
+
+public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    Optional<Frase> obterFraseAleatoria();
 }
