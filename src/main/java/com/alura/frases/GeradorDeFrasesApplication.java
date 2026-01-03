e4ae8b0521b71e4b270f8add759181da81e906a5
Luiz <luizzlh2004@gmail.com>
2026-01-03 15:14:44 -0300

    Inicio do exercício

---
+++ src/main/java/com/alura/frases/GeradorDeFrasesApplication.java
@@ -0,0 +1,13 @@
+package com.alura.frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeFrasesApplication.class, args);
+	}
+
+}
