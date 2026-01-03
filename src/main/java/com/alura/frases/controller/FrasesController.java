b6ce24dea1eb2450b9b1d60808a6a2ca20529abd
Luiz <luizzlh2004@gmail.com>
2026-01-03 15:30:31 -0300

    Criando classe model Frases

---
+++ src/main/java/com/alura/frases/controller/FrasesController.java
@@ -0,0 +1,4 @@
+package com.alura.frases.controller;
+
+public class FrasesController {
+}
---
+++ src/main/java/com/alura/frases/models/Frases.java
@@ -0,0 +1,59 @@
+package com.alura.frases.models;
+
+public class Frases {
+    private Long id;
+    private String titulo;
+    private String frase;
+    private String personagem;
+    private String poster;
+
+    public Frases(){}
+
+    public Frases(Long id, String titulo, String frase, String personagem, String poster) {
+        this.id = id;
+        this.titulo = titulo;
+        this.frase = frase;
+        this.personagem = personagem;
+        this.poster = poster;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getPersonagem() {
+        return personagem;
+    }
+
+    public void setPersonagem(String personagem) {
+        this.personagem = personagem;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
